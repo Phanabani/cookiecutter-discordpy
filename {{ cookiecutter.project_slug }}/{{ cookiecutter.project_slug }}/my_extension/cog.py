0aed4767a122b1256e44757c17e05296059c0328
Phanabani <phanabani@gmail.com>
2022-03-02 00:59:47 -0500

    Add ping command.

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/my_extension/cog.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/my_extension/cog.py
@@ -14,3 +14,7 @@ class MyCog(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
+    @commands.command()
+    async def ping(self, context: commands.Context):
+        await context.reply('Pong!')
